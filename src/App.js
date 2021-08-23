feca5e267621cee571825fcc541f8961f7eded08
tony.cho <tony.cho@wavebridgelabs.com>
2021-08-23 14:16:44 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Web Analytics Test</div>
+      <button>testButton</button>
+    </div>
+  );
+}
+
+export default App;
