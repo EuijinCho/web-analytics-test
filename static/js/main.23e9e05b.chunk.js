aea71428422c2bba334a640da172806f809e4ec8
tony.cho <tony.cho@wavebridgelabs.com>
2021-08-23 17:41:29 +0900

    Updates

--- static/js/main.23e9e05b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpweb-analytics-test"]=this["webpackJsonpweb-analytics-test"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);e(1),e(4),e(8),e.p,e(9),e(0);(function(t){t&&t instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(n){var e=n.getCLS,i=n.getFID,c=n.getFCP,s=n.getLCP,o=n.getTTFB;e(t),i(t),c(t),s(t),o(t)}))})()},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.23e9e05b.chunk.js.map
